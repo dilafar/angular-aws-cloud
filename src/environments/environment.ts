5439bf94ddb4faf80a199a7d51df18f5e098ec17
dilafar <it20784720@my.sliit.lk>
2024-11-26 06:36:13 +0000

    remote commit from jenkins

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiServerUrl: "ebs-ansible-jar.us-east-1.elasticbeanstalk.com/api"
+  apiServerUrl: "backend-employee.us-east-1.elasticbeanstalk.com/api"
 };
